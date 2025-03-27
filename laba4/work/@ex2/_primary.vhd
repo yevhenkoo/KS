629f69d70189a14814143565d3e5f4ad8f936a5a
yevhenkoo <157299776+yevhenkoo@users.noreply.github.com>
2025-03-27 21:53:19 +0200

    Add files via upload

---
+++ laba4/work/@ex2/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity Ex2 is
+    port(
+        x1              : in     vl_logic;
+        x2              : in     vl_logic;
+        x3              : in     vl_logic;
+        f1              : out    vl_logic;
+        f2              : out    vl_logic
+    );
+end Ex2;
---
+++ laba4/work/@ex2_@o@r/_primary.vhd
@@ -0,0 +1,9 @@
+library verilog;
+use verilog.vl_types.all;
+entity Ex2_OR is
+    port(
+        x1              : in     vl_logic;
+        x2              : in     vl_logic;
+        f               : out    vl_logic
+    );
+end Ex2_OR;
